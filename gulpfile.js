b0d71a2806d9a5f41560d60096f5f2d6fda337c8
Ravi <ravipgupta1989@hotmail.com>
2016-10-21 18:51:49 +0530

    Fixed error when using the min version of js file

--- gulpfile.js
+++ gulpfile.js
@@ -7,21 +7,32 @@ var gulp = require('gulp'),
   ngModuleSort = require('gulp-ng-module-sort'),
   sort = require('gulp-angular-filesort'),
   browserify = require('gulp-browserify'),
+  order = require('gulp-order'),
   concat = require('gulp-concat');
 
 gulp.task('scripts', function () {
   return gulp.src(['C:/Users/ravi/Desktop/Mission Impossible Project/nwjs-sdk-v0.17.0-win-ia32/MI/gulpdependencies/angular.js',
-  'C:/Users/ravi/Desktop/Mission Impossible Project/nwjs-sdk-v0.17.0-win-ia32/MI/gulpdependencies/angular-ui-router.js'
-    ,'C:/Users/ravi/Desktop/Mission Impossible Project/nwjs-sdk-v0.17.0-win-ia32/MI/gulpdependencies/*.js'])
+      'C:/Users/ravi/Desktop/Mission Impossible Project/nwjs-sdk-v0.17.0-win-ia32/MI/gulpdependencies/angular-ui-router.js', 'C:/Users/ravi/Desktop/Mission Impossible Project/nwjs-sdk-v0.17.0-win-ia32/MI/gulpdependencies/*.js'
+    ])
     .pipe(jshint('.jshintrc'))
-    .pipe(sort())
+    //.pipe(sort())
     .pipe(jshint.reporter('default'))
+    .pipe(order(['gulpdependencies/_angular.js',
+      'gulpdependencies/ui-bootstrap.js',
+      'gulpdependencies/ui-bootstrap-tpls.js',
+      'gulpdependencies/angular-ui-router.js',
+      'gulpdependencies/angular-animate.js',
+      'gulpdependencies/alertProvider.js',
+      'gulpdependencies/satellizer.js'
+    ], {
+      base: './'
+    }))
     .pipe(concat('main.js'))
     .pipe(gulp.dest('gulp/js'))
     .pipe(rename({
       suffix: '.min'
     }))
-    .pipe(uglify())
+    .pipe(uglify({mangle: false}))
     .pipe(gulp.dest('gulp/js'))
     .pipe(notify({
       message: 'Scripts task complete'
