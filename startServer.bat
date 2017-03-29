c93b84865e7f0ba56b4bd14cca6eed383cc97d4e
Learn-Ravi <ravipgupta1989@hotmail.com>
2017-03-29 15:29:24 +0530

    Update startServer.bat

--- startServer.bat
+++ startServer.bat
@@ -1,3 +1,3 @@
 @echo off
-REM C:\Users\i96765\AppData\Roaming\npm\gulp
+C:\Users\i96765\AppData\Roaming\npm\pm2 stop server\server3.js
 C:\Users\i96765\AppData\Roaming\npm\pm2 start server\server3.js
