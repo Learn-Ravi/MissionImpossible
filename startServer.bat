1531fc683963c6e5755ae028647ce206f1c9fbee
Learn-Ravi <ravipgupta1989@hotmail.com>
2017-03-29 15:33:06 +0530

    Update startServer.bat

--- startServer.bat
+++ startServer.bat
@@ -1,3 +1,2 @@
 @echo off
-C:\Users\i96765\AppData\Roaming\npm\pm2 stop server\server3.js
-C:\Users\i96765\AppData\Roaming\npm\pm2 start server\server3.js
+C:\Users\i96765\AppData\Roaming\npm\pm2 restart server\server3.js
